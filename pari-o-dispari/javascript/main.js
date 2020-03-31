5dc43fbbba68f617e6a99318abf7269ff99a6959
Luca Stefani <theblackloki@gmail.com>
2020-03-31 14:40:20 +0200

    fine esercizio

--- pari-o-dispari/javascript/main.js
+++ pari-o-dispari/javascript/main.js
@@ -1,40 +1,31 @@
-// Creo gioco pari o dispari casuale tra due computer
+// Creo gioco pari o dispari
 
 var pariDispariPC = Math.floor(Math.random() * 2)
 
 if (pariDispariPC = 0) {
-  console.log("Il Computer 1 è pari")
-  console.log("Il Computer 2 è dispari")
+  alert("Il Computer 1 è pari")
+  alert("Il Computer 2 è dispari")
 }else {
-  console.log("Il Computer 1 è dispari")
-  console.log("Il Computer 2 è pari")
+  alert("Il Computer 1 è dispari")
+  alert("Il Computer 2 è pari")
 }
-// Generiamo due numeri da 1 a 5 per i computer
+// Generiamo due numeri da 1 a 5 per il pc
 
 var primoNumero = numberGenerator(1,5)
 var secondoNumero = numberGenerator(1,5)
-console.log("Il numero del Computer 1 è " + primoNumero);
-console.log("Il numero del Computer 2 è " + secondoNumero);
+console.log(primoNumero);
+console.log(secondoNumero);
 
- // Sommiamo i due numeri e dichiariamo se è pari o dispari
+ // Sommiamo i due numeri e dichiariamo chi ha vinto.
 
 var somma = primoNumero + secondoNumero
-
-console.log("La somma dei numeri è " + somma);
-
+console.log(somma);
 var finale = pari(somma);
-if (finale===true) {
-  console.log("il numero è pari");
+console.log(finale);
+if (finale = true && pariDispariPC = 0 || finale = false && pariDispariPC = 1) {
+  alert("Ha vinto il Computer 1")
 }else {
-  console.log("il numero è dispari");
-}
-
-// Decretiamo il vincitore
-
-if (finale===true & pariDispariPC===1||finale===false && pariDispariPC===0) {
-  console.log("Ha vinto il Computer 1")
-}else {
-  console.log("Ha vinto il Computer 2")
+  alert("Ha vinto il Computer 2")
 }
 
  // Creo funzioni per il generatore dei numeri e per il check sul pari e dispari
